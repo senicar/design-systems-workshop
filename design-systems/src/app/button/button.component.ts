a53e7a5bab7713ea1547928a62c505159dd975ca
Andreas Wissel <github@kaeku.io>
2022-09-02 14:09:02 +0200

    lab-4

--- design-systems/src/app/button/button.component.ts
+++ design-systems/src/app/button/button.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() public label: string = 'Button Label';
+  @Input() public type: 'primary' | 'secondary' = 'primary';
 
   constructor() {}
 
