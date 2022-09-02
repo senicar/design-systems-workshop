293da295a0c28f255a2c85eeeec0cacadb9c0c79
Andreas Wissel <github@kaeku.io>
2022-09-02 23:01:14 +0200

    lab-10

---
+++ design-systems/src/app/form-layout/form-layout.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormLayoutComponent } from './form-layout.component';
+
+
+
+@NgModule({
+  declarations: [
+    FormLayoutComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FormLayoutModule { }
