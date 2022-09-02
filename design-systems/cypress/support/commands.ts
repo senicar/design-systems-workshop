9b8def9df6b6f9042ba49da5fa4d9a2cfd52b12d
Andreas Wissel <github@kaeku.io>
2022-09-02 16:57:57 +0200

    lab-7

---
+++ design-systems/cypress/support/commands.ts
@@ -0,0 +1,90 @@
+// ***********************************************
+// This example namespace declaration will help
+// with Intellisense and code completion in your
+// IDE or Text Editor.
+// ***********************************************
+// declare namespace Cypress {
+//   interface Chainable<Subject = any> {
+//     customCommand(param: any): typeof customCommand;
+//   }
+// }
+//
+// function customCommand(param: any): void {
+//   console.warn(param);
+// }
+//
+// NOTE: You can use it like so:
+// Cypress.Commands.add('customCommand', customCommand);
+//
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+import { buildArgsParam } from '@storybook/router';
+
+/// <reference types="cypress" />
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      visitComponentStory(
+        componentName: string,
+        storyName: string
+      ): Chainable<AUTWindow>;
+      visitComponentStoryWithArgs(
+        componentName: string,
+        storyName: string,
+        args: { [key: string]: any }
+      ): Chainable<AUTWindow>;
+    }
+  }
+}
+
+function visitComponentStory(componentName: string, storyName: string) {
+  const url = `localhost:6006/iframe.html?id=components-${componentName}--${storyName}`;
+
+  return cy.visit(url);
+}
+
+function visitComponentStoryWithArgs(
+  componentName: string,
+  storyName: string,
+  args?: { [key: string]: any }
+) {
+  const url = `localhost:6006/iframe.html?id=components-${componentName}--${storyName}`;
+
+  if (!args) {
+    return cy.visit(url);
+  } else {
+    const urlWithArgs = `${url}&args=${buildArgsParam({}, args)}`;
+    return cy.visit(urlWithArgs);
+  }
+}
+
+Cypress.Commands.add('visitComponentStory', visitComponentStory);
+Cypress.Commands.add(
+  'visitComponentStoryWithArgs',
+  visitComponentStoryWithArgs
+);
+
+export {};
